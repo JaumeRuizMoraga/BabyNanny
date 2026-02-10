4a90afa7fe7c9a254e161aef3ed4e23f2baec93f
Alberto P <alpegom2001@gmail.com>
2026-02-10 19:33:47 +0100

    Cargando proyecto api

---
+++ BabyNannyAPI/src/main/java/babbynanny/babynannyapi/BabyNannyApiApplication.java
@@ -0,0 +1,13 @@
+package babbynanny.babynannyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyNannyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyNannyApiApplication.class, args);
+	}
+
+}
