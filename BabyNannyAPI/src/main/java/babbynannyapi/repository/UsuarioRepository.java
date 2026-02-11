21d885a924d134d97805ca8d166446aa4a5a2ee5
Alberto P <alpegom2001@gmail.com>
2026-02-11 19:59:57 +0100

    API buena

---
+++ BabyNannyAPI/src/main/java/babbynannyapi/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+/*
+package babbynannyapi.repository;
+
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface UsuarioRepository extends MongoRepository<Record, String> {
+
+    @Query(value = "{ 'dificultad': ?0 }")
+    List<Record> buscarDificultat(int valor);
+
+}
+*/
