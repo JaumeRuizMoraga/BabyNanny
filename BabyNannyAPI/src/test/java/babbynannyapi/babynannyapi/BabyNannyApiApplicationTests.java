6489b9e6fe9831dd4826fe84c72a0dc4b24b491e
Alberto P <alpegom2001@gmail.com>
2026-02-11 20:14:24 +0100

    commit 3

---
+++ BabyNannyAPI/src/test/java/babbynannyapi/babynannyapi/BabyNannyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package babbynannyapi.babynannyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyNannyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
